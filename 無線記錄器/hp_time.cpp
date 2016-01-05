eb57cace98b8a7662f547748a67a8f74a03dd4e4
popoC <poasmile@gmail.com>
2016-01-05 16:29:46 +0800

    v.0105

---
+++ 無線記錄器/hp_time.cpp
@@ -0,0 +1,254 @@
+#include "hp_time.h"
+#include "stdio.h" 
+#include "string.h"
+
+/***************************************************************************
+ * ms_timestr2hptime:
+ * 
+ * Convert a generic time string to a high precision epoch time.
+ * SEED time format is "YYYY[/MM/DD HH:MM:SS.FFFF]", the delimiter can
+ * be a dash [-], slash [/], colon [:], or period [.] and between the
+ * date and time a 'T' or a space may be used.  The fracttional
+ * seconds must begin with a period [.].
+ *
+ * The time string can be "short" in which case the omitted values are
+ * assumed to be zero (with the exception of month and day which are
+ * assumed to be 1): "YYYY/MM/DD" assumes HH, MM, SS and FFFF are 0.
+ * The year is required, otherwise there wouldn't be much for a date.
+ *
+ * Ranges are checked for each value.
+ *
+ * Returns epoch time on success and HPTERROR on error.
+ ***************************************************************************/
+hptime_t
+ms_timestr2hptime (char *timestr)
+{
+  int fields;
+  int year = 0;
+  int mon  = 1;
+  int mday = 1;
+  int day  = 1;
+  int hour = 0;
+  int min  = 0;
+  int sec  = 0;
+  float fusec = 0.0;
+  //double fusec = 0.0;
+  signed long usec = 0;
+    
+  fields = sscanf(timestr, "%d%*[-/:.]%d%*[-/:.]%d%*[-/:.T ]%d%*[-/:.]%d%*[- /:.]%d%f",&year, &mon, &mday, &hour, &min, &sec, &fusec);
+  
+  /* Convert fractional seconds to microseconds */
+  if ( fusec != 0.0 )
+    {
+      usec = (signed long)(fusec * 1000000.0 + 0.5);
+    }
+
+  if ( fields < 1 )
+    {
+      //ms_log (2, "ms_timestr2hptime(): Error converting time string: %s\n", timestr);
+      return HPTERROR;
+    }
+  
+  if ( year < 1900 || year > 3000 )
+    {
+      //ms_log (2, "ms_timestr2hptime(): Error with year value: %d\n", year);
+      return HPTERROR;
+    }
+  
+  if ( mon < 1 || mon > 12 )
+    {
+     //ms_log (2, "ms_timestr2hptime(): Error with month value: %d\n", mon);
+      return HPTERROR;
+    }
+
+  if ( mday < 1 || mday > 31 )
+    {
+     // ms_log (2, "ms_timestr2hptime(): Error with day value: %d\n", mday);
+      return HPTERROR;
+    }
+
+  /* Convert month and day-of-month to day-of-year */
+  if ( ms_md2doy(year, mon, mday, &day) )
+    {
+      return HPTERROR;
+    }
+  
+  if ( hour < 0 || hour > 23 )
+    {
+     // ms_log (2, "ms_timestr2hptime(): Error with hour value: %d\n", hour);
+      return HPTERROR;
+    }
+  
+  if ( min < 0 || min > 59 )
+    {
+     // ms_log (2, "ms_timestr2hptime(): Error with minute value: %d\n", min);
+      return HPTERROR;
+    }
+  
+  if ( sec < 0 || sec > 60 )
+    {
+   //   ms_log (2, "ms_timestr2hptime(): Error with second value: %d\n", sec);
+      return HPTERROR;
+    }
+  
+  if ( usec < 0 || usec > 999999 )
+    {
+   //   ms_log (2, "ms_timestr2hptime(): Error with fractional second value: %d\n", usec);
+      return HPTERROR;
+    }
+  
+  return ms_time2hptime_int(year, day, hour, min, sec, usec);
+}  /* End of ms_timestr2hptime() */
+
+
+//------------------------------------------------------------------------------
+/***************************************************************************
+ * ms_md2doy:
+ *
+ * Compute the day-of-year from a year, month and day-of-month.
+ *
+ * Year is expected to be in the range 1900-2100, month is expected to
+ * be in the range 1-12, mday is expected to be in the range 1-31 and
+ * jday will be in the range 1-366.
+ *
+ * Returns 0 on success and -1 on error.
+ ***************************************************************************/
+int
+ms_md2doy(int year, int month, int mday, int *jday)
+{
+  int idx;
+  int leap;
+  int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  
+  /* Sanity check for the supplied parameters */
+  if ( year < 1900 || year > 2100 )
+    {
+    //  ms_log (2, "ms_md2doy(): year (%d) is out of range\n", year);
+      return -1;
+    }
+  if ( month < 1 || month > 12 )
+    {
+    //  ms_log (2, "ms_md2doy(): month (%d) is out of range\n", month);
+      return -1;
+    }
+  if ( mday < 1 || mday > 31 )
+    {
+    //  ms_log (2, "ms_md2doy(): day-of-month (%d) is out of range\n", mday);
+      return -1;
+    }
+  
+  /* Test for leap year */
+  leap = ( ((year%4 == 0) && (year%100 != 0)) || (year%400 == 0) ) ? 1 : 0;
+  
+  /* Add a day to February if leap year */
+  if ( leap )
+    days[1]++;
+  
+  /* Check that the day-of-month jives with specified month */
+  if ( mday > days[month-1] )
+    {
+   //   ms_log (2, "ms_md2doy(): day-of-month (%d) is out of range for month %d\n", mday, month);
+      return -1;
+    }
+
+  *jday = 0;
+  month--;
+  
+  for ( idx=0; idx < 12; idx++ )
+    {
+      if ( idx == month )
+	{
+	  *jday += mday;
+	  break;
+	}
+      
+      *jday += days[idx];
+    }
+  
+  return 0;
+}  /* End of ms_md2doy() */
+
+/***************************************************************************
+ * ms_time2hptime_int:
+ *
+ * Convert specified time values to a high precision epoch time.  This
+ * is an internal version which does no range checking, it is assumed
+ * that checking the range for each value has already been done.
+ *
+ * Returns epoch time on success and HPTERROR on error.
+ ***************************************************************************/
+static hptime_t
+ms_time2hptime_int (int year, int day, int hour, int min, int sec, int usec)
+{
+  BTime btime;
+  hptime_t hptime;
+  
+  memset(&btime, 0, sizeof(BTime));
+  btime.day = 1;
+  
+  /* Convert integer seconds using ms_btime2hptime */
+  btime.year = (int16_t)year;
+  btime.day = (int16_t)day;
+  btime.hour = (uint8_t)hour;
+  btime.min = (uint8_t)min;
+  btime.sec = (uint8_t)sec;
+  btime.fract = 0;
+
+  hptime = ms_btime2hptime(&btime);
+  
+  if ( hptime == HPTERROR )
+    {
+     // ms_log (2, "ms_time2hptime(): Error converting with ms_btime2hptime()\n");
+      return HPTERROR;
+    }
+  
+  /* Add the microseconds */
+  hptime += (hptime_t)usec * (1000000 / HPTMODULUS);
+  
+  return hptime;
+}  /* End of ms_time2hptime_int() */
+
+/***************************************************************************
+ * ms_btime2hptime:
+ *
+ * Convert a binary SEED time structure to a high precision epoch time
+ * (1/HPTMODULUS second ticks from the epoch).  The algorithm used is
+ * a specific version of a generalized function in GNU glibc.
+ *
+ * Returns a high precision epoch time on success and HPTERROR on
+ * error.
+ ***************************************************************************/
+hptime_t
+ms_btime2hptime (BTime *btime)
+{
+  hptime_t hptime;
+  
+  signed long shortyear;
+  signed long a4, a100, a400;
+  signed long intervening_leap_days;
+  signed long days;
+
+    /*
+  int shortyear;
+  int a4, a100, a400;
+  int intervening_leap_days;
+  int days;
+  */
+  if ( ! btime )
+    return HPTERROR;
+  
+  shortyear = btime->year - 1900;
+
+  a4 = (shortyear >> 2) + 475 - ! (shortyear & 3);
+  a100 = a4 / 25 - (a4 % 25 < 0);
+  a400 = a100 >> 2;
+  intervening_leap_days = (a4 - 492) - (a100 - 19) + (a400 - 4);
+  
+  days = (365 * (shortyear - 70) + intervening_leap_days + (btime->day - 1));
+  
+  hptime = (hptime_t )(60 * (60 * (24 * days + btime->hour) + btime->min) + btime->sec) * HPTMODULUS
+    + (btime->fract * (HPTMODULUS / 10000));
+    
+  return hptime;
+}  /* End of ms_btime2hptime() */
+
---
+++ 無線記錄器/main.cpp
@@ -0,0 +1,482 @@
+//-------------P6---------------------------------------------------------------
+//       P1.5
+//         |
+//      ___↓_                    ________________                   _____
+//     |      |P1.3  ->  P6.1    |                |<---  Rx        |     |
+//     |      |P1.4  ->  P6.2    |                |--->  Tx (COM2) |  PC |
+//     |      |P1.1  <-  P6.3    |                |--->  Gnd       |_____|
+//     | 2013 |P1.2  <-  P6.4    |    430f5436    |                 ______        
+//     |      |                  |                |<---Tx          |     |
+//     |      |                  |                |--->Rx  (COM1)  |GPS  |
+//     |______|P1.7  <-  P6.7    |________________|--->Gnd         |_____|
+//------------------------------------------------------------------------------
+// 2016 0104 for 土石流~  20Mhz - 100Hz
+#include  <msp430x54xA.h>
+#include <stdio.h>
+//#include "main.h"
+#include "hp_time.h"
+#include "string.h"
+
+#define  COM1 0x01
+#define  COM2 0x02
+
+void Crystal_Init(); 
+void UART_Init(char com);
+void RS232_Send_Char(char *str,int count,char com);
+
+int UART_COM1_RX_count,UART_COM2_RX_count;
+unsigned char UART_COM2_RX_BUF[128];
+
+char COM1_BUFFER[128];    // -- 115200 msp430
+char COM1_REC_BUFFER[128];    // -- receive buffer
+
+static hptime_t sysTime;
+#define BUF_SIZE 10
+
+
+char GPS_Time_String[] = "2000/12/22 10:12:22.000000";
+
+int CloCk_10KHz =0;
+
+void findStrPoint(char *a,char *ans,char feature,int n);
+
+hptime_t get_hp_Gps_time(char *tstr){
+
+   char gpsinfo[10];
+   hptime_t s1;
+
+  if( strncmp("$GPRMC",tstr,6)==0){
+  
+       
+    
+       findStrPoint(tstr,gpsinfo ,',',3);
+       if(gpsinfo[0]=='A'){ //--- 這裡開始數據有效
+           findStrPoint(tstr,gpsinfo,',',2);
+        
+            GPS_Time_String[11] =  gpsinfo[0];
+            GPS_Time_String[12] =  gpsinfo[1];
+            GPS_Time_String[14] =  gpsinfo[2];
+            GPS_Time_String[15] =  gpsinfo[3];
+            GPS_Time_String[17] =  gpsinfo[4];
+            GPS_Time_String[18] =  gpsinfo[5];
+           
+           findStrPoint(tstr,gpsinfo,',',10);
+
+            GPS_Time_String[2] =  gpsinfo[4];
+            GPS_Time_String[3] =  gpsinfo[5];
+            GPS_Time_String[5] =  gpsinfo[2];
+            GPS_Time_String[6] =  gpsinfo[3];
+            GPS_Time_String[8] =  gpsinfo[0];
+            GPS_Time_String[9] =  gpsinfo[1];
+        
+            
+            sprintf(&GPS_Time_String[20],"%04d",CloCk_10KHz);
+            s1 = ms_timestr2hptime(GPS_Time_String);       
+           //s1 = ms_timestr2hptime("1980/12/22 10:12:22.016803");       
+           
+           
+           return s1;       
+       }
+       else{
+           return 0;
+       }
+  }
+  else{
+  
+      return 0;
+  }
+
+}
+
+//----------for rs232-----------------------------------------------------------
+char COM2_Command[128];
+char string[150];
+void delay_ms(int del);
+
+//----------for ADS1222---------------------------------------------------------
+unsigned long DATA[4]={0} ;                        //資料暫存區
+bool ADS1222_Interrupt_Flag = false;
+   void ADS1222Init();
+   void ADS1222_STANDBY_MODE();
+    int ADS1222_SELF_CALIBRATION();
+                     int First_Data = 0;
+                     int DRDY_ready = 0;
+                     int BITN[8] = {0x0001,0x0002,0x0004,0x0008,0x0010,0x0020,0x0040,0x0080};
+  
+                 
+//-----------for GPS------------------------------------------------------------
+                     
+                    
+                    
+                     
+//------------------------------------------------------------------------------                     
+int GPS_count = 0;
+//int nTX1_Len = 16;
+void main(void)
+{
+   WDTCTL = WDTPW + WDTHOLD;                    // Stop WDT
+   
+   P6DIR |= (BIT3+BIT4+BIT7);      //out   BIT7 很重要
+   P6DIR &= ~(BIT1+BIT2+BIT6);     //input
+   P6OUT &= ~(BIT4+BIT3+BIT7); 
+   
+   
+   P1DIR = (BIT0 +BIT1+BIT7) ;
+   P1OUT |= BIT0;                           //   爺亨版
+
+   Crystal_Init();                          // 震盪器初始化
+   UART_Init(COM2+COM1);                         // Rs232初始化    
+   ADS1222Init();                           // ADS1222初始化
+ 
+  static hptime_t nowTime;
+  while(1){
+           __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, enable interrupts 此處中斷大約100Hz
+           
+           
+             for(int d1elAy=0;d1elAy<1600;d1elAy++);    //--delay 
+                if(First_Data < 5){ First_Data += ADS1222_SELF_CALIBRATION();}
+                else{ ADS1222_STANDBY_MODE(); }
+           
+     nowTime = 0;    
+     nowTime  =   sysTime;//get_hp_Gps_time(COM1_BUFFER);    
+             
+                
+     string[0] = '$';                         //資料型態定義
+     string[1] = '@';                         //資料型態定義     
+     string[2] = GPS_count >> 8;                         //資料型態定義
+     string[3] = GPS_count;                         //資料型態定義     
+     string[4] =  (DATA[0]   >>  16 ) & 0xff ;
+     string[5] =  (DATA[0]   >>  8 ) & 0xff;
+     string[6] =  (DATA[0]    ) & 0xff;
+     string[7] =  (DATA[1]   >>  16 ) & 0xff ;
+     string[8] =  (DATA[1]   >>  8 ) & 0xff;
+     string[9] =  (DATA[1]    ) & 0xff;
+     string[10] =  (DATA[2]   >>  16 ) & 0xff ;
+     string[11] =  (DATA[2]   >>  8 ) & 0xff;
+     string[12] =  (DATA[2]    ) & 0xff;
+     string[13] =  0;
+     string[14] =  '!';
+     string[15] =  '#';
+     //nTX1_Len = 16;
+    //  RS232_Send_Char(string,16,COM2);  
+       RS232_Send_Char(string,16,COM2);  
+      
+        
+  }
+  //-------------------------------- start record-----------------------------
+  
+ 
+  
+//   __no_operation();                          // For debugger                   
+}
+//----------------------------------------------------------------------------
+void Crystal_Init(){
+    //--啟動 20MHZ 與32767HZ
+    P5SEL |= 0x0C;                             // Port select XT2
+    P7SEL |= 0x03;                             // Port select XT1
+    UCSCTL6 &= ~(XT1OFF + XT2OFF);            // Set XT1 & XT2 On
+    UCSCTL6 |= XCAP_3;                        // Internal load cap
+
+    do    // Loop until XT1,XT2 & DCO stabilizes
+    {
+      UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + XT1HFOFFG + DCOFFG);
+                                              // Clear XT2,XT1,DCO fault flags
+      SFRIFG1 &= ~OFIFG;                      // Clear fault flags
+    }while (SFRIFG1&OFIFG);                   // Test oscillator fault flag
+
+    UCSCTL6 &= ~XT2DRIVE0;                    // Decrease XT2 Drive according to expected frequency
+    UCSCTL4 |= SELA_0 + SELS_5 + SELM_5 ;      // Select SMCLK, ACLK source and DCO source
+
+  P4SEL = 0x02;                               // P4 option select 
+  P4DIR = 0x02;                               // P4 outputs  1.6666MHz.......
+ 
+  P1DIR |=BIT6;
+  P1SEL |= BIT6; 
+    
+  TBCCR0 = 5;                                 // PWM Period
+  TBCCTL1 = OUTMOD_4;                         // toggle
+  TBCCR1 = 2;                                 // CCR1 PWM Duty Cycle	
+  
+  TBCTL = TBSSEL_2 + MC_1 + TBCLR;            // SMCLK, upmode, clear TBR 
+  
+   TA0CCTL0 = CCIE;                          // CCR0 interrupt enabled
+   TA0CCR0 = 2000-1;
+   TA0CTL = TASSEL_2 + MC_1 + TACLR ;         // SMCLK, contmode, clear TAR
+}
+//--------------------------------------------------------------------------------
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void TIMER0_A0_ISR(void)
+{
+  CloCk_10KHz++;
+  if(CloCk_10KHz>10000){
+    CloCk_10KHz=0;
+    
+    P1OUT ^= BIT7;
+  } 
+  
+  
+}
+//------------------------------------------------------------------------------
+void ADS1222Init()
+{
+                            //------------P2------------------------------------
+                            // P2.0(O) P2.1(I) P2.2(I) P2.3(I) P2.4(I) P2.5(I) P2.6(I) P2.7(I)
+                            //   ↓      ↑      ↑      ↑      ↑      ↑      ↑      ↑
+                            //  SCLK    CH1     CH2     CH3     CH4     CH5     CH6     CH7
+                            //        interrupt
+                            //------------P2------------------------------------
+  P2SEL = 0;
+  P2DIR |= BIT0;                                   //set P2.0 to output direction
+  P2DIR &= ~(BIT1+BIT2+BIT3+BIT4);  //set P2.1~2.7 to get data on channel N
+  P2OUT &= ~(BIT0);                                //set P2.0 to SCLK   (low)+
+  P2IE  = 0 ;                                      //
+  P2IES =0;                                        //
+  P2IFG =0;                                        //
+  P2IES |= (BIT1+BIT2+BIT3+BIT4);                         //選擇中斷模式(1 負緣觸發 )
+  P2IE  |= (BIT1+BIT2+BIT3+BIT4);                         // Set P2.1 interrupt   ~加入判斷全部通道是否準備好資料
+  
+  for(long int i=0;i<159600;i++){}
+
+  _EINT();  //啟動全域中斷
+}
+//------------------------------------------------------------------------------
+//-------------以下這段  請依據示波器  對儀器做最佳的調整-----------------------
+#pragma vector=PORT2_VECTOR
+__interrupt void P2ISR (void)
+{
+  if((P2IFG & BIT1) ==BIT1)          {    DRDY_ready |= BIT1;P2IFG &= ~(BIT1); }
+  if((P2IFG & BIT2) ==BIT2)          {    DRDY_ready |= BIT2;P2IFG &= ~(BIT2); }
+  if((P2IFG & BIT3) ==BIT3)          {    DRDY_ready |= BIT3;P2IFG &= ~(BIT3); }
+  if((P2IFG & BIT4) ==BIT4)          {    DRDY_ready |= BIT4;P2IFG &= ~(BIT4); }
+
+  if(DRDY_ready != (BIT3+BIT2+BIT1+BIT4)) {  
+     return; 
+  } 
+
+  DRDY_ready = 0;
+  GPS_count++;
+  if(GPS_count>=200)GPS_count=0;
+  
+                P2IFG &= ~(BIT3+BIT2+BIT1+BIT4);                   // 中斷旗號暫存器(0 無中斷 1 中斷等待處理)         
+                __bic_SR_register_on_exit(LPM0_bits); // Exit LPM0
+    
+}
+//------------------------------------------------------------------------------
+int ADS1222_SELF_CALIBRATION()
+{
+int scLk,ch_n,delAy;
+long delay_for_calibration;
+  for(scLk=1;scLk<=24;scLk++){
+    P2OUT |= (BIT0) ;                  //--同步要求輸出資料(SCLK)-----------上源觸發
+    for(delAy=0;delAy<20;delAy++);                //DELAY
+
+       for(ch_n=0; ch_n<4; ch_n++){
+           if((P2IN&BITN[ch_n+1])==BITN[ch_n+1]){ DATA[ch_n] =( DATA[ch_n]<< 1) | 1;}
+           else{ DATA[ch_n] =( DATA[ch_n]<< 1) | 0;}
+       }
+
+    P2OUT &= ~(BIT0) ;                  //--輸出資料結束--------------通知AD資料收集結束
+    for(delAy=0;delAy<30;delAy++);
+  }
+    P2OUT |= (BIT0)  ;                  //--25th SCLK UP
+    for(delAy=0;delAy<30;delAy++);    
+    P2OUT &= ~(BIT0) ;                  //--25th SCLK DOWN
+    for(delAy=0;delAy<30;delAy++);
+
+    
+    P2OUT |= (BIT0)  ;                  //--26th SCLK UP
+    for(delAy=0;delAy<30;delAy++);    
+    P2OUT &= ~(BIT0) ;                  //--26th SCLK DOWN
+    for(delAy=0;delAy<30;delAy++);
+ 
+    
+    
+    P2OUT |= (BIT0)  ;                  //--26th SCLK UP
+    for(delay_for_calibration=0;delay_for_calibration<76000;delay_for_calibration++);    //--SELF CALIBRATION & STANDBY MODE    
+    P2OUT &= ~(BIT0) ;
+    
+    return(1);
+}
+//------------------------------------------------------------------------------
+void ADS1222_STANDBY_MODE(){
+int scLk,ch_n,delAy;
+
+  for(scLk=1;scLk<=24;scLk++){
+    P2OUT |= (BIT0) ;                  //--同步要求輸出資料(SCLK)-----------上源觸發
+    for(delAy=0;delAy<3;delAy++);      //DELAY  3*0.25us = 0.75us >> 100ns((P.14- ads1222)
+    
+       for(ch_n=0; ch_n<4; ch_n++){
+           if((P2IN&BITN[ch_n+1])==BITN[ch_n+1]){ DATA[ch_n] =( DATA[ch_n]<< 1) | 1;}
+           else{ DATA[ch_n] =( DATA[ch_n]<< 1) | 0;}
+       }
+
+   if(scLk!=24){
+    P2OUT &= ~(BIT0) ;                  //--輸出資料結束--------------通知AD資料收集結束
+    for(delAy=0;delAy<25;delAy++);
+   }
+  }
+
+   for(ch_n=0; ch_n<4; ch_n++){
+     if( (DATA[ch_n]&0x00FFFFFF) <= 0x007FFFFF){      //----2010 / 08/17 修改AD輸出格式 讓輸出0點附近連續
+         DATA[ch_n] += 0x00800000; 
+     }
+     else{
+         DATA[ch_n] -= 0x00800000;
+     }
+   }
+ 
+  //if(1){
+   for(delAy=0;delAy<1500;delAy++);    //--STANDBY MODE
+   P2OUT &= ~(BIT0) ;
+ // }
+       
+}
+//------------------------------------------------------------------------------
+
+void delay_ms(int del){
+int dt;
+  for(int delAy=0;delAy<del;delAy++)    //--delay 
+    for(dt=0;dt<4000;dt++);
+}
+//----------------------------------------------------------------------------
+void UART_Init(char com){
+   //須要先啟動 XT2 16MHZ
+  if( (com&COM1) == COM1 ){
+          
+      P3SEL = 0x30;                             // P3.4,5 = USCI_A0 TXD/RXD
+      UCA0CTL1 |= UCSWRST;                      // **Put state machine in reset**
+
+      UCA0CTL1 |= UCSSEL_1;                     // CLK = ACLK = 32767
+      UCA0BR0 = 0x03;                           //   (see User's Guide)  9600
+      UCA0BR1 = 0x00;                           //
+
+      UCA0MCTL = UCBRS_3+UCBRF_0;               // Modulation UCBRSx=2, UCBRFx=0
+
+      UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+      UCA0IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+  }
+  if( (com&COM2) == COM2 ){
+
+      P5SEL |= 0xc0;                             // P5.6,7 = USCI_A0 TXD/RXD
+      UCA1CTL1 |= UCSWRST;                      // **Put state machine in reset**
+
+      UCA1CTL1 |= UCSSEL_2;                     // CLK = SCLK
+    
+        UCA1BR0 = 0xAD; 
+        UCA1BR1 = 0x00;
+
+
+      UCA1MCTL = UCBRS_5+UCBRF_0;               // Modulation UCBRSx=3, UCBRFx=0
+
+      UCA1CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+//      UCA1IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+  }
+
+  UART_COM1_RX_count = 0;
+  UART_COM2_RX_count = 0;
+
+  
+}
+//----------------------------------------------------------------------------
+#pragma vector=USCI_A0_VECTOR  //COM1
+__interrupt void USCI_A0_ISR(void)
+{
+  switch(__even_in_range(UCA0IV,4))
+  {
+    case 2:                                   // Vector 2 - RXIFG
+      COM1_REC_BUFFER[UART_COM1_RX_count] = UCA0RXBUF;
+        UART_COM1_RX_count++;
+    
+       if(COM1_REC_BUFFER[UART_COM1_RX_count-1] == '\n')
+       {
+         if(strncmp("$GPRMC",COM1_REC_BUFFER,6)==0){
+            memcpy(COM1_BUFFER,COM1_REC_BUFFER,UART_COM1_RX_count+1);
+         }
+            memset(COM1_REC_BUFFER,0,UART_COM1_RX_count+1);
+            UART_COM1_RX_count = 0;
+       }
+    
+    if(UART_COM1_RX_count>=128)UART_COM1_RX_count=0;   
+      
+  break;
+  default: break;  
+  }
+    __bic_SR_register_on_exit(LPM0_bits); // Exit LPM0
+}
+//----------------------------------------------------------------------------
+#pragma vector=USCI_A1_VECTOR //COM2
+__interrupt void USCI_A1_ISR(void)
+{
+  
+  //---命令格式    @xm..........$
+  //---            @ 開始字元
+  //---            x 令命字數
+  //---            m 命令模式
+  //---            .....  命令
+  //---            $ 結束字元   
+  
+  switch(__even_in_range(UCA1IV,4))
+  {
+  case 2:                                   // Vector 2 - RXIFG
+//    while (!(UCA1IFG&UCTXIFG));           // USCI_A1 TX buffer ready?
+//    UCA1TXBUF = UCA1RXBUF;                // TX -> RXed character
+    UART_COM2_RX_BUF[UART_COM2_RX_count] = UCA1RXBUF;
+    UART_COM2_RX_count++;
+    if(UART_COM2_RX_count>=128)UART_COM2_RX_count=0;   
+     
+    break;
+  default: break;  
+  }
+ //   __bic_SR_register_on_exit(LPM3_bits); // Exit LPM0
+}
+//----------------------------------------------------------------------------
+void UART_SendByte(unsigned char data,char com)
+{
+  if((com&COM1) == COM1){
+    while (!(UCA0IFG&UCTXIFG));             // USCI_A1 TX buffer ready?
+    UCA0TXBUF = data;
+  }
+  if((com&COM2) == COM2){
+    while (!(UCA1IFG&UCTXIFG));             // USCI_A1 TX buffer ready?
+    UCA1TXBUF = data;
+  }
+  
+}
+void UART_SendStr(char *str,char com)
+{
+        while (*str != '\0')
+        {
+            UART_SendByte(*str++,com);	          // 發送數據
+        }
+        
+}
+void RS232_Send_Char(char *str,int count,char com)
+{
+        for(int i=0;i<count;i++)
+        {
+            UART_SendByte(*str++,com);	          // 發送數據
+        }
+}
+
+//-----------------------------------------------------------------------------
+void findStrPoint(char *a,char *ans,char feature,int n){
+      int strcount = 0, Ncount = 0, pop = 0;
+      while(a[strcount]!='\0')
+      {
+         if(a[strcount]== feature){
+            pop++;
+            if(pop==n)
+            {
+             ans[Ncount]='\0';
+             return;
+            }
+            Ncount = 0;
+         }
+         else{
+          ans[Ncount] = a[strcount];
+          Ncount++;
+         }
+          strcount++;
+      }
+        ans[Ncount]='\0';
+}
